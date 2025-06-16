7ee02153c398ed525b0d5973b939fbad45e52567
ibrahdz <iyhernandezo@gmail.com>
2025-06-16 16:57:00 -0500

    Update after a long work day

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 import { Suspense } from 'react';
- 
+
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
@@ -9,12 +9,12 @@ export default function LoginPage() {
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
             <AcmeLogo />
-          </div>
+          </div>          
         </div>
         <Suspense>
           <LoginForm />
         </Suspense>
-      </div>
+      </div>        
     </main>
   );
 }
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -6,6 +6,7 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
+import { signIn } from "@/auth"
 
 export default function LoginForm() {
   return (
@@ -55,9 +56,17 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full">
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Button>
+        <form
+          action={async () => {
+            "use server"
+            await signIn("google")
+          }}
+        >
+          <Button className="mt-4 w-full" type="submit">
+            Sign in with Google
+            <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+          </Button>
+        </form>
         <div className="flex h-8 items-end space-x-1">
           {/* Add form errors here */}
         </div>
